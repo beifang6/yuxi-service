5d66b94ad5eef65cd6548aefcd62dd0ddf2a1529
13255288043 <xiping0318@163.com>
2021-01-13 13:23:55 +0800

    first

---
+++ src/views/login.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="login_css">
+    <div class="login_box">
+      <div class="avator_box">
+        <img src="../assets/yuxi.jpg" />
+      </div>
+      <el-form ref="form" :model="form" label-width="0px" class="login_form">
+        <el-form-item>
+          <el-input v-model="form.name" placeholder="请输入用户名"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="form.name" placeholder="请输入密码"></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button type="primary" @click="onSubmit">登录</el-button>
+          <el-button>取消</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      //表单数据
+      form: {},
+    };
+  },
+  methods: {
+    onSubmit() {},
+  },
+};
+</script>
+<style scoped>
+.login_css {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login_box {
+  height: 300px;
+  width: 450px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+.avator_box {
+  width: 130px;
+  height: 130px;
+  border: 1px solid #eee;
+  border-radius: 50%;
+  padding: 10px;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+}
+img {
+  width: 130px;
+  height: 130px;
+  border-radius: 50%;
+  background-color: #eee;
+}
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+.login_form {
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+'use strict'
+module.exports = {
+  devServer: {
+    host: '0.0.0.0',
+    port: 1123
+  }
+}
